0fb262a1861e8e6591a57049ea4ae44b271ac9d3
Paweł Stopczyński <stopek.pawel@gmail.com>
2023-02-09 12:03:04 +0100

    access to ContentStreamer

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ContentStreamer} from "../../language/streamer/content-streamer.service";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
 })
 export class AboutComponent {
-
+  constructor(private content: ContentStreamer) {
+    console.log(content.select('navigator.test'))
+  }
 }
