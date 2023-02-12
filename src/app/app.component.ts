cbe88bcfeaaba8e4a27b221f41ac6d2988717363
stopek <stopek.pawel@gmail.com>
2023-02-12 14:00:21 +0100

    clear code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-// import { NavigationEnd, Router } from '@angular/router';
-// import { CookieService } from 'ngx-cookie-service';
-//
-// import { ContentSelector } from './core/modules/content/router/content-selector.service';
-// import { RouterService } from './core/modules/content/router/router.service';
-import { ContentStreamer } from './core/modules/content/streamer/content-streamer.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-  // private cookieService: CookieService,
-  // private routerService: RouterService,
-  // private contentSelector: ContentSelector,
-  // private router: Router
-  constructor(private contentStreamer: ContentStreamer) {}
-
   ngOnInit() {
-    console.log(
-      `w AppComponent.ngOnInit lang: ${this.contentStreamer.language}`
-    );
+    console.log('AppComponent!');
   }
 }
